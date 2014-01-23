60fca51e115ad53232feabca6b18d6e0f7de1bcd
Alexis Fajardo Moya <afmoya@uclv.cu>
2014-01-23 14:29:00 -0500

    Implementados los métodos que devuelven el buffer de la capa raster para las transformaciones que se le realizan a la capa en la interfaz (zoom, till, paning, etc.).
    Implementado el método para leer un subconjunto de filas del archivo de
    datos (org.gvsig.raster.dataset.RasterDataset#readBlock(int, int, int)).
    
    Implementado el método para leer una línea completa del archivo de
    datos (org.gvsig.raster.driver.RasterDataset#readCompletetLine(int,
    int)).

--- src/uclv/gvsig/extsdf/raster/NetCDFRasterDataset.java
+++ src/uclv/gvsig/extsdf/raster/NetCDFRasterDataset.java
@@ -26,6 +26,8 @@
 package uclv.gvsig.extsdf.raster;
 
 import java.awt.geom.AffineTransform;
+import java.awt.geom.NoninvertibleTransformException;
+import java.awt.geom.Point2D;
 import java.io.File;
 import java.io.IOException;
 
@@ -44,6 +46,8 @@ import org.gvsig.raster.dataset.properties.DatasetColorInterpretation;
 import org.gvsig.raster.dataset.properties.DatasetMetadata;
 import org.gvsig.raster.datastruct.Extent;
 import org.gvsig.raster.datastruct.Transparency;
+import org.gvsig.raster.process.RasterTask;
+import org.gvsig.raster.process.RasterTaskQueue;
 import org.gvsig.raster.util.extensionPoints.ExtensionPoint;
 
 import uclv.gvsig.extsdf.NetCDFController;
@@ -141,7 +145,7 @@ public class NetCDFRasterDataset extends RasterDataset {
 					- controller.getSelectedLat().getMaxValue())
 					/ (controller.getSelectedLat().getSize() - 1);
 			// Verifica si las latitudes aumentan o decrementean
-			if (controller.getSelectedLat().getCoordEdge(0) != min)
+			if (controller.getSelectedLat().getCoordValue(0) != min)
 				incLat = -incLat;
 
 			// Calcula el incremento de la longitud
@@ -150,7 +154,7 @@ public class NetCDFRasterDataset extends RasterDataset {
 					- controller.getSelectedLon().getMaxValue())
 					/ (controller.getSelectedLon().getSize() - 1);
 			// Verifica si las latitudes aumentan o decrementean
-			if (controller.getSelectedLon().getCoordEdge(0) != min)
+			if (controller.getSelectedLon().getCoordValue(0) != min)
 				incLon = -incLon;
 
 			// Inicializa las transformacions
@@ -265,50 +269,178 @@ public class NetCDFRasterDataset extends RasterDataset {
 	}
 
 	/**
-	 * (non javadoc)
+	 * Devuelve el b&uacute;ffer del Raster
+	 * 
+	 * Esta funci&oacute;n es llamada cuando se muestra el mapa en su
+	 * tama&ntilde;o normal o cuando se le aumenta el zoom
 	 * 
 	 * @see org.gvsig.raster.dataset.RasterDataset#getWindowRaster(double,
-	 *      double, double, double, BandList, IBuffer)
+	 *      double, double, double, org.gvsig.raster.dataset.BandList,
+	 *      org.gvsig.raster.dataset.IBuffer)
 	 */
 	@Override
 	public IBuffer getWindowRaster(double ulx, double uly, double lrx,
 			double lry, BandList bandList, IBuffer rasterBuf)
 			throws InterruptedException, RasterDriverException {
-		// TODO Auto-generated method stub
-		return null;
+		// Calcula los puntos diagonales del rectángulo representado en la vista
+		Point2D p1 = new Point2D.Double(ulx, uly);
+		Point2D p2 = new Point2D.Double(lrx, lry);
+
+		// Calula las filas y columnas iniciales y finales de la vista
+		int iRow, iCol, eRow, eCol;
+		try {
+			externalTransformation.inverseTransform(p1, p1);
+			externalTransformation.inverseTransform(p2, p2);
+			iCol = (int) Math.min(p1.getX(), p2.getX());
+			iRow = (int) Math.min(p1.getY(), p2.getY());
+			eCol = (int) Math.max(p1.getX(), p2.getX());
+			eRow = (int) Math.max(p1.getY(), p2.getY());
+			ownTransformation.transform(p1, p1);
+			ownTransformation.transform(p2, p2);
+		} catch (NoninvertibleTransformException e) {
+			throw new RasterDriverException("Noninvertible transform");
+		}
+
+		// Establece la vista según las nuevas dimensiones
+		Extent selectedExtent = new Extent(p1, p2);
+		setView(selectedExtent);
+
+		// Calcula los incrementos por filas y columnas
+		double incCol = (double) (eCol - iCol - 1)
+				/ (double) rasterBuf.getWidth();
+		double incRow = (double) (eRow - iRow - 1)
+				/ (double) rasterBuf.getHeight();
+
+		// Carga los datos de la vista
+		try {
+			controller.readDataToBuffer(rasterBuf, iRow, iCol, incRow, incCol);
+		} catch (Exception e) {
+			throw new RasterDriverException(
+					"Formato de archivo NetCDF no soportado");
+		}
+		return rasterBuf;
 	}
 
 	/**
-	 * (non javadoc)
+	 * Devuelve el b&uacute;ffer del Raster
+	 * 
+	 * <i>No detectada la operaci&oacute;n que la dispara</i>
 	 * 
 	 * @see org.gvsig.raster.dataset.RasterDataset#getWindowRaster(double,
 	 *      double, double, double, BandList, IBuffer, boolean)
 	 */
 	@Override
-	public IBuffer getWindowRaster(double x, double y, double w, double h,
+	public IBuffer getWindowRaster(double ulx, double uly, double w, double h,
 			BandList bandList, IBuffer rasterBuf, boolean adjustToExtent)
 			throws InterruptedException, RasterDriverException {
-		// TODO Auto-generated method stub
-		return null;
+		// Calcula las dimensiones de la capa Raster
+		Extent ext = getExtent();
+		Point2D pInit = rasterToWorld(new Point2D.Double(0, 0));
+		Point2D pEnd = rasterToWorld(new Point2D.Double(getWidth(), getHeight()));
+		double wRaster = Math.abs(pEnd.getX() - pInit.getX());
+		double hRaster = Math.abs(pEnd.getY() - pInit.getY());
+		double lrx = (((ext.getULX() - wRaster) > ext.maxX()) || ((ext.getULX() - wRaster) < ext
+				.minX())) ? (ulx + w) : (ulx - w);
+		double lry = (((ext.getULY() - hRaster) > ext.maxY()) || ((ext.getULY() - hRaster) < ext
+				.minY())) ? (uly + h) : (uly - h);
+
+		// Calcula los puntos diagonales del rectángulo representado en la vista
+		Point2D p1 = new Point2D.Double(ulx, uly);
+		Point2D p2 = new Point2D.Double(lrx, lry);
+
+		// Calula las filas y columnas iniciales y finales de la vista
+		int iRow, iCol, eRow, eCol;
+		try {
+			externalTransformation.inverseTransform(p1, p1);
+			externalTransformation.inverseTransform(p2, p2);
+			iCol = (int) Math.min(p1.getX(), p2.getX());
+			iRow = (int) Math.min(p1.getY(), p2.getY());
+			eCol = (int) Math.max(p1.getX(), p2.getX());
+			eRow = (int) Math.max(p1.getY(), p2.getY());
+			ownTransformation.transform(p1, p1);
+			ownTransformation.transform(p2, p2);
+		} catch (NoninvertibleTransformException e) {
+			throw new RasterDriverException("Noninvertible transform");
+		}
+
+		// Establece la vista según las nuevas dimensiones
+		Extent selectedExtent = new Extent(p1.getX(), p1.getY(), p2.getX(),
+				p2.getY());
+		setView(selectedExtent);
+
+		// Calcula los incrementos por filas y columnas
+		double incCol = (double) (eCol - iCol - 1)
+				/ (double) rasterBuf.getWidth();
+		double incRow = (double) (eRow - iRow - 1)
+				/ (double) rasterBuf.getHeight();
+
+		// Carga los datos de la vista
+		try {
+			controller.readDataToBuffer(rasterBuf, iRow, iCol, incRow, incCol);
+		} catch (Exception e) {
+			throw new RasterDriverException(
+					"Formato de archivo NetCDF no soportado");
+		}
+		return rasterBuf;
 	}
 
 	/**
-	 * (non javadoc)
+	 * Devuelve el b&uacute;ffer del Raster
+	 * 
+	 * Este función es llamada cuando se muestra el mapa en su tama&ntilde;o
+	 * peque&ntilde;o, o sea, cuando se le disminuye el zoom.
 	 * 
 	 * @see org.gvsig.raster.dataset.RasterDataset#getWindowRaster(double,
 	 *      double, double, double, int, int, BandList, IBuffer, boolean)
 	 */
 	@Override
-	public IBuffer getWindowRaster(double minX, double minY, double maxX,
-			double maxY, int bufWidth, int bufHeight, BandList bandList,
+	public IBuffer getWindowRaster(double ulx, double uly, double lrx,
+			double lry, int bufWidth, int bufHeight, BandList bandList,
 			IBuffer rasterBuf, boolean adjustToExtent)
 			throws InterruptedException, RasterDriverException {
-		// TODO Auto-generated method stub
-		return null;
+		// Calcula los puntos diagonales del rectángulo representado en la vista
+		Point2D p1 = new Point2D.Double(ulx, uly);
+		Point2D p2 = new Point2D.Double(lrx, lry);
+
+		// Calula las filas y columnas iniciales y finales de la vista
+		int iRow, iCol, eRow, eCol;
+		try {
+			externalTransformation.inverseTransform(p1, p1);
+			externalTransformation.inverseTransform(p2, p2);
+			iCol = (int) Math.min(p1.getX(), p2.getX());
+			iRow = (int) Math.min(p1.getY(), p2.getY());
+			eCol = (int) Math.max(p1.getX(), p2.getX());
+			eRow = (int) Math.max(p1.getY(), p2.getY());
+			ownTransformation.transform(p1, p1);
+			ownTransformation.transform(p2, p2);
+		} catch (NoninvertibleTransformException e) {
+			throw new RasterDriverException("Noninvertible transform");
+		}
+
+		// Establece la vista según las nuevas dimensiones
+		Extent selectedExtent = new Extent(p1, p2);
+		setView(selectedExtent);
+
+		// Calcula los incrementos por filas y columnas
+		double incCol = (double) (eCol - iCol - 1)
+				/ (double) rasterBuf.getWidth();
+		double incRow = (double) (eRow - iRow - 1)
+				/ (double) rasterBuf.getHeight();
+
+		// Carga los datos de la vista
+		try {
+			controller.readDataToBuffer(rasterBuf, iRow, iCol, incRow, incCol);
+		} catch (Exception e) {
+			throw new RasterDriverException(
+					"Formato de archivo NetCDF no soportado");
+		}
+		return rasterBuf;
 	}
 
 	/**
-	 * (non javadoc)
+	 * Devuelve el b&uacute;ffer del Raster
+	 * 
+	 * <i>No detectada la operaci&oacute;n que la dispara</i>
 	 * 
 	 * @see org.gvsig.raster.dataset.RasterDataset#getWindowRaster(int, int,
 	 *      int, int, BandList, IBuffer)
@@ -317,13 +449,51 @@ public class NetCDFRasterDataset extends RasterDataset {
 	public IBuffer getWindowRaster(int x, int y, int w, int h,
 			BandList bandList, IBuffer rasterBuf) throws InterruptedException,
 			RasterDriverException {
-		// TODO Auto-generated method stub
-		return null;
+		// Calcula los puntos diagonales del rectángulo representado en la vista
+		Point2D init = this.rasterToWorld(new Point2D.Double(x, y));
+		Point2D end = this.rasterToWorld(new Point2D.Double(x + w, y + h));
+
+		// Calula las filas y columnas iniciales y finales de la vista
+		int iRow, iCol, eRow, eCol;
+		try {
+			externalTransformation.inverseTransform(init, init);
+			externalTransformation.inverseTransform(end, end);
+			iCol = (int) Math.min(init.getX(), end.getX());
+			iRow = (int) Math.min(init.getY(), end.getY());
+			eCol = (int) Math.max(init.getX(), end.getX());
+			eRow = (int) Math.max(init.getY(), end.getY());
+			ownTransformation.transform(init, init);
+			ownTransformation.transform(end, end);
+		} catch (NoninvertibleTransformException e) {
+			throw new RasterDriverException("Noninvertible transform");
+		}
+
+		// Establece la vista según las nuevas dimensiones
+		Extent selectedExtent = new Extent(init.getX(), init.getY(),
+				end.getX(), end.getY());
+		setView(selectedExtent);
+
+		// Calcula los incrementos por filas y columnas
+		double incCol = (double) (eCol - iCol - 1)
+				/ (double) rasterBuf.getWidth();
+		double incRow = (double) (eRow - iRow - 1)
+				/ (double) rasterBuf.getHeight();
+
+		// Carga los datos de la vista
+		try {
+			controller.readDataToBuffer(rasterBuf, iRow, iCol, incRow, incCol);
+		} catch (Exception e) {
+			throw new RasterDriverException(
+					"Formato de archivo NetCDF no soportado");
+		}
+		return rasterBuf;
 	}
 
 	/**
-	 * (non javadoc)
+	 * Devuelve el b&uacute;ffer del Raster
 	 * 
+	 * <i>No detectada la operaci&oacute;n que la dispara</i>
+	 *   
 	 * @see org.gvsig.raster.dataset.RasterDataset#getWindowRaster(int, int,
 	 *      int, int, int, int, BandList, IBuffer)
 	 */
@@ -331,8 +501,44 @@ public class NetCDFRasterDataset extends RasterDataset {
 	public IBuffer getWindowRaster(int x, int y, int w, int h, int bufWidth,
 			int bufHeight, BandList bandList, IBuffer rasterBuf)
 			throws InterruptedException, RasterDriverException {
-		// TODO Auto-generated method stub
-		return null;
+		// Calcula los puntos diagonales del rectángulo representado en la vista
+		Point2D init = this.rasterToWorld(new Point2D.Double(x, y));
+		Point2D end = this.rasterToWorld(new Point2D.Double(x + w, y + h));
+
+		// Calula las filas y columnas iniciales y finales de la vista
+		int iRow, iCol, eRow, eCol;
+		try {
+			externalTransformation.inverseTransform(init, init);
+			externalTransformation.inverseTransform(end, end);
+			iCol = (int) Math.min(init.getX(), end.getX());
+			iRow = (int) Math.min(init.getY(), end.getY());
+			eCol = (int) Math.max(init.getX(), end.getX());
+			eRow = (int) Math.max(init.getY(), end.getY());
+			ownTransformation.transform(init, init);
+			ownTransformation.transform(end, end);
+		} catch (NoninvertibleTransformException e) {
+			throw new RasterDriverException("Noninvertible transform");
+		}
+
+		// Establece la vista según las nuevas dimensiones
+		Extent selectedExtent = new Extent(init.getX(), init.getY(),
+				end.getX(), end.getY());
+		setView(selectedExtent);
+
+		// Calcula los incrementos por filas y columnas
+		double incCol = (double) (eCol - iCol - 1)
+				/ (double) rasterBuf.getWidth();
+		double incRow = (double) (eRow - iRow - 1)
+				/ (double) rasterBuf.getHeight();
+
+		// Carga los datos de la vista
+		try {
+			controller.readDataToBuffer(rasterBuf, iRow, iCol, incRow, incCol);
+		} catch (Exception e) {
+			throw new RasterDriverException(
+					"Formato de archivo NetCDF no soportado");
+		}
+		return rasterBuf;
 	}
 
 	/**
@@ -355,21 +561,90 @@ public class NetCDFRasterDataset extends RasterDataset {
 	public Object readCompleteLine(int line, int band)
 			throws InvalidSetViewException, FileNotOpenException,
 			RasterDriverException, InterruptedException {
-		// TODO Auto-generated method stub
-		return null;
+		// Verifica que la línea se válida
+		if (line == controller.getHeight() || band == getBandCount())
+			throw new InvalidSetViewException("Request out of grid");
+
+		// Lee una línea en dependencia del tipo de datos
+		switch (controller.getDataType()) {
+		case IBuffer.TYPE_DOUBLE:
+			return controller.readLineDouble(line);
+		case IBuffer.TYPE_FLOAT:
+			return controller.readLineFloat(line);
+		case IBuffer.TYPE_INT:
+			return controller.readLineInteger(line);
+		case IBuffer.TYPE_SHORT:
+			return controller.readLineShort(line);
+		default:
+			return controller.readLineByte(line);
+		}
 	}
 
 	/**
-	 * (non javadoc)
+	 * Devuelve un subconjunto de filas del archiv de datos
 	 * 
+	 * @param pos
+	 *            fila inicial
+	 * @param blockHeight
+	 *            cantidad de filas a leer
+	 * 
+	 * @return datos del archivo NetCDF
+	 * 
+	 * @throws InvalidSetViewException
+	 * @throws FileNotOpenException
+	 * @throws RasterDriverException
+	 * @throws InterruptedException
 	 * @see org.gvsig.raster.dataset.RasterDataset#readBlock(int, int)
 	 */
 	@Override
 	public Object readBlock(int pos, int blockHeight)
 			throws InvalidSetViewException, FileNotOpenException,
 			RasterDriverException, InterruptedException {
-		// TODO Auto-generated method stub
-		return null;
+		RasterTask task = RasterTaskQueue
+				.get(Thread.currentThread().toString());
+
+		try {
+			controller.calculaMinMax();
+		} catch (Exception e) {
+			new RasterDriverException("Formato de archivo NetCDF no soportado");
+		}
+
+		// Verifica que la posición sea válida
+		if (pos < 0)
+			throw new InvalidSetViewException("Request out of grid");
+
+		// Verifica que la cantidad de filas a leer sea válida
+		if ((pos + blockHeight) > getHeight())
+			blockHeight = Math.abs(getHeight() - pos);
+
+		// Lee los datos en dependencia del tipo de dato del NetCDF
+		switch (controller.getDataType()) {
+		// Datos tipo byte
+		case IBuffer.TYPE_BYTE:
+			if (task.getEvent() != null)
+				task.manageEvent(task.getEvent());
+			return controller.readBlockByte(pos, blockHeight);
+			// Datos tipo short
+		case IBuffer.TYPE_SHORT:
+			if (task.getEvent() != null)
+				task.manageEvent(task.getEvent());
+			return controller.readBlockShort(pos, blockHeight);
+			// Datos tipo int
+		case IBuffer.TYPE_INT:
+			if (task.getEvent() != null)
+				task.manageEvent(task.getEvent());
+			return controller.readBlockInteger(pos, blockHeight);
+			// Datos tipo float
+		case IBuffer.TYPE_FLOAT:
+			if (task.getEvent() != null)
+				task.manageEvent(task.getEvent());
+			return controller.readBlockFloat(pos, blockHeight);
+			// Datos tipo double
+		default:
+			if (task.getEvent() != null)
+				task.manageEvent(task.getEvent());
+			return controller.readBlockDouble(pos, blockHeight);
+		}
 	}
 
 	/**
