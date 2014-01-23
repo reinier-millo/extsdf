d486d8b300ce681480bc0f2514d3d147312f588f
Reinier Millo Sánchez <rmillo@uclv.cu>
2014-01-23 14:44:28 -0500

    Actualizando extensión para dar soporte al formato de archivo NetCDF.
    Signed-off-by: Reinier Millo Sánchez <rmillo@uclv.cu>

--- src/uclv/gvsig/extsdf/driver/NetCDFDriverExtension.java
+++ src/uclv/gvsig/extsdf/driver/NetCDFDriverExtension.java
@@ -26,17 +26,11 @@
 
 package uclv.gvsig.extsdf.driver;
 
-import org.gvsig.raster.gui.preferences.RasterPreferences;
-import org.gvsig.rastertools.properties.RasterPropertiesTocMenuEntry;
-import org.gvsig.rastertools.properties.panels.BandSelectorPanel;
-import org.gvsig.rastertools.properties.panels.EnhancedPanel;
-import org.gvsig.rastertools.properties.panels.GeneralPanel;
-import org.gvsig.rastertools.properties.panels.InfoPanel;
-import org.gvsig.rastertools.properties.panels.TransparencyPanel;
-
 import uclv.gvsig.extsdf.properties.PropertiesNetCDFRasterTocMenuEntry;
 import uclv.gvsig.extsdf.properties.panels.NetCDFPanel;
 import uclv.gvsig.extsdf.properties.panels.TimePanel;
+import uclv.gvsig.extsdf.raster.NetCDFRasterDataset;
+import uclv.gvsig.extsdf.testRaster.MMMC;
 
 import com.iver.andami.plugins.Extension;
 import com.iver.utiles.extensionPoints.ExtensionPoint;
@@ -47,67 +41,71 @@ import com.iver.utiles.extensionPoints.ExtensionPointsSingleton;
  * Extensi&oacute;n para darle soporte al Driver para leer archivos NetCDF
  * 
  * @author millo
- * @version 1.0.0
+ * @version 1.0.1
  * 
  */
 public class NetCDFDriverExtension extends Extension {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.iver.andami.plugins.IExtension#execute(java.lang.String)
-	 */
-	@Override
-	public void execute(String arg0) {
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.iver.andami.plugins.IExtension#execute(java.lang.String)
+     */
+    @Override
+    public void execute(String arg0) {
+    }
 
-	/**
-	 * Inicializa la extensi&oacute;n
-	 * 
-	 * @see com.iver.andami.plugins.IExtension#initialize()
-	 */
-	@Override
-	public void initialize() {
-		// Obtiene todos los puntos de extensión del gvSIG
-		ExtensionPoints extensionPoints = ExtensionPointsSingleton
-				.getInstance();
+    /**
+     * Inicializa la extensi&oacute;n
+     * 
+     * @see com.iver.andami.plugins.IExtension#initialize()
+     */
+    @Override
+    public void initialize() {
+        // Obtiene todos los puntos de extensión del gvSIG
+        ExtensionPoints extensionPoints = ExtensionPointsSingleton
+                .getInstance();
 
-		// Creación del punto de extensión para registrar paneles en el cuadro
-		// de propiedades.
-		if (!extensionPoints.containsKey("NetCDFPropertiesDialog")) {
-			extensionPoints
-					.put(new ExtensionPoint(
-							"NetCDFPropertiesDialog",
-							"NetCDF Properties registrable panels (register instances of javax.swing.JPanel)"));
-		}
-		
-		// Añadimos paneles al cuadro de propiedades.
-		extensionPoints.add("NetCDFPropertiesDialog", "NetCDF", NetCDFPanel.class);
-		extensionPoints.add("NetCDFPropertiesDialog", "time",TimePanel.class);
-		
-		// Añadimos las entradas del menú del toc de raster
-		extensionPoints.add("View_TocActions", "NetCDFProperties",
-				PropertiesNetCDFRasterTocMenuEntry.getSingleton());
-	}
+        // Creación del punto de extensión para registrar paneles en el cuadro
+        // de propiedades.
+        if (!extensionPoints.containsKey("NetCDFPropertiesDialog")) {
+            extensionPoints
+                    .put(new ExtensionPoint(
+                            "NetCDFPropertiesDialog",
+                            "NetCDF Properties registrable panels (register instances of javax.swing.JPanel)"));
+        }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.iver.andami.plugins.IExtension#isEnabled()
-	 */
-	@Override
-	public boolean isEnabled() {
-		return true;
-	}
+        // Añadimos paneles al cuadro de propiedades.
+        extensionPoints.add("NetCDFPropertiesDialog", "NetCDF",
+                NetCDFPanel.class);
+        extensionPoints.add("NetCDFPropertiesDialog", "time", TimePanel.class);
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.iver.andami.plugins.IExtension#isVisible()
-	 */
-	@Override
-	public boolean isVisible() {
-		return false;
-	}
+        // Añadimos las entradas del menú del toc de raster
+        extensionPoints.add("View_TocActions", "NetCDFProperties",
+                PropertiesNetCDFRasterTocMenuEntry.getSingleton());
+
+        // Registra el driver para dar soporte a los archivos NetCDF
+        NetCDFRasterDataset.registerDriver();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.iver.andami.plugins.IExtension#isEnabled()
+     */
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.iver.andami.plugins.IExtension#isVisible()
+     */
+    @Override
+    public boolean isVisible() {
+        return false;
+    }
 
 }
