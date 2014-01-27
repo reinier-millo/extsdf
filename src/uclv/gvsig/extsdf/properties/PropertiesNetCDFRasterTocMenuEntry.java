f94e540d73493b320272ca557c9d4fa6956eefb5
Daynier Cardoso Roque <dcardoso@uclv.edu.cu>
2014-01-27 10:45:38 -0500

    Mejorando el javadoc

--- src/uclv/gvsig/extsdf/properties/PropertiesNetCDFRasterTocMenuEntry.java
+++ src/uclv/gvsig/extsdf/properties/PropertiesNetCDFRasterTocMenuEntry.java
@@ -1,4 +1,3 @@
-
 /*
  * PropertiesNetCDFRaster.java
  *
@@ -44,89 +43,165 @@ import com.iver.cit.gvsig.project.documents.view.toc.AbstractTocContextMenuActio
 import com.iver.cit.gvsig.project.documents.view.toc.ITocItem;
 
 /**
- * Clase que posibilita la entrada en el menú contextual del TOC correspondiente al cuadro de
- * propiedades del raster 
+ * Clase que posibilita la entrada en el menú contextual del TOC correspondiente
+ * al cuadro de propiedades del raster
  * 
  * @author dcardoso
- * @see {@link AbstractTocContextMenuAction}, {@link IGenericToolBarMenuItem}
- *
+ * @see link AbstractTocContextMenuAction
+ * @see IGenericToolBarMenuItem
+ * 
  */
-/**
- * @author dcardoso
- *
- */
-public class PropertiesNetCDFRasterTocMenuEntry extends 	AbstractTocContextMenuAction
-												implements 	IGenericToolBarMenuItem{
+public class PropertiesNetCDFRasterTocMenuEntry extends
+		AbstractTocContextMenuAction implements IGenericToolBarMenuItem {
 
-	static private PropertiesNetCDFRasterTocMenuEntry 	singleton  = null;
-	private PanelGroupDialog                    		properties = null;                         
-	FLayer 													   lyr = null;
+	static private PropertiesNetCDFRasterTocMenuEntry singleton = null;
+	private PanelGroupDialog properties = null;
+	FLayer lyr = null;
 	/**
-	 * Variable para controlar si los eventos de los paneles se deben interpretar.
-	 * En la carga inicial se deben desactivar todos los eventos
+	 * Variable para controlar si los eventos de los paneles se deben
+	 * interpretar. En la carga inicial se deben desactivar todos los eventos
 	 */
-	public static boolean                     enableEvents = false;
-	
+	public static boolean enableEvents = false;
+
 	/**
 	 * Nadie puede crear una instancia a esta clase unica, hay que usar el
 	 * getSingleton()
 	 */
-	private PropertiesNetCDFRasterTocMenuEntry() {}
+	private PropertiesNetCDFRasterTocMenuEntry() {
+	}
 
 	/**
 	 * Devuelve un objeto unico de esta clase
+	 * 
 	 * @return PropertiesNetCDFRasterTocMenuEntry
 	 */
 	static public PropertiesNetCDFRasterTocMenuEntry getSingleton() {
 		if (singleton == null)
 			singleton = new PropertiesNetCDFRasterTocMenuEntry();
 		return singleton;
-	}	
-	
-	/*
-	 * (non-Javadoc)
+	}
+
+	/**
+	 * Devuelve el nombre del grupo al que pertenece el ítem de menú
+	 * 
 	 * @see com.iver.cit.gvsig.project.documents.contextMenu.AbstractContextMenuAction#getGroup()
+	 * @see org.gvsig.raster.gui.IGenericToolBarMenuItem#getGroup()
 	 */
+	@Override
 	public String getGroup() {
 		return "RasterLayer";
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 * Devuelve el orden del grupo
+	 * 
 	 * @see com.iver.cit.gvsig.project.documents.contextMenu.AbstractContextMenuAction#getGroupOrder()
+	 * @see org.gvsig.raster.gui.IGenericToolBarMenuItem#getGroupOrder()
 	 */
+	@Override
 	public int getGroupOrder() {
 		return 60;
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 * Devuelve orden del ítem
+	 * 
 	 * @see com.iver.cit.gvsig.project.documents.contextMenu.AbstractContextMenuAction#getOrder()
+	 * @see org.gvsig.raster.gui.IGenericToolBarMenuItem#getOrder()
 	 */
+	@Override
 	public int getOrder() {
 		return 0;
 	}
 
-	/* (non-Javadoc)
-	 * @see com.iver.cit.gvsig.project.documents.IContextMenuAction#getText()
+	/**
+	 * Devuelve el texto que debe mostrar el ítem del menú
+	 * 
+	 * @see com.iver.cit.gvsig.project.documents.contextMenu.AbstractContextMenuAction#getText()
+	 * @see org.gvsig.raster.gui.IGenericToolBarMenuItem#getText()
 	 */
 	@Override
 	public String getText() {
-		return PluginServices.getText(this,"NetCDF_properties");
+		return PluginServices.getText(this, "NetCDF_properties");
 	}
 
 	/**
-	 * Gestiona la apertura del dialogo de propiedades del NetCDF 
+	 * Devuelve el icono del ítem del menú
 	 * 
+	 * @see com.iver.cit.gvsig.project.documents.contextMenu.AbstractContextMenuAction#getIcon()
+	 * @see org.gvsig.raster.gui.IGenericToolBarMenuItem#getIcon()
 	 */
-	/* (non-Javadoc)
-	 * @see com.iver.cit.gvsig.project.documents.view.toc.AbstractTocContextMenuAction#execute(com.iver.cit.gvsig.project.documents.view.toc.ITocItem, com.iver.cit.gvsig.fmap.layers.FLayer[])
+	@Override
+	public Icon getIcon() {
+		return RasterToolsUtil.getIcon("properties-icon");
+	}
+
+	/**
+	 * Verifica si el menú puede habilitarse o no
+	 * 
+	 * @param item
+	 *            elemento del menu
+	 * @param selectedItems
+	 *            conjunto de capas seleccionadas
+	 * 
+	 * @return <b>true</b> si la capa seleccionada es un raster de un NetCDF<br />
+	 *         <b>false</b> en cualquier otro caso
+	 * 
+	 * @see com.iver.cit.gvsig.project.documents.view.toc.AbstractTocContextMenuAction#isVisible(com.iver.cit.gvsig.project.documents.view.toc.ITocItem,
+	 *      com.iver.cit.gvsig.fmap.layers.FLayer[])
+	 */
+	public boolean isEnabled(ITocItem item, FLayer[] selectedItems) {
+		if ((selectedItems == null) || (selectedItems.length != 1))
+			return false;
+		FLayer lyr = selectedItems[0];
+		if (lyr instanceof FLyrRasterSE) {
+			FLyrRasterSE fr = (FLyrRasterSE) lyr;
+			return fr.getDataSource().getDataset(0)[0] instanceof NetCDFRasterDataset;
+		}
+		return false;
+	}
+
+	/**
+	 * Verifica si el menú puede mostrarse o no
+	 * 
+	 * @param item
+	 *            elemento del menu
+	 * @param selectedItems
+	 *            conjunto de capas seleccionadas
+	 * 
+	 * @return <b>true</b> si la capa seleccionada es un raster de un NetCDF<br />
+	 *         <b>false</b> en cualquier otro caso
+	 * 
+	 * @see com.iver.cit.gvsig.project.documents.view.toc.AbstractTocContextMenuAction#isVisible(com.iver.cit.gvsig.project.documents.view.toc.ITocItem,
+	 *      com.iver.cit.gvsig.fmap.layers.FLayer[])
+	 */
+	public boolean isVisible(ITocItem item, FLayer[] selectedItems) {
+		if ((selectedItems == null) || (selectedItems.length != 1))
+			return false;
+		FLayer lyr = selectedItems[0];
+		if (lyr instanceof FLyrRasterSE) {
+			FLyrRasterSE fr = (FLyrRasterSE) lyr;
+			return fr.getDataSource().getDataset(0)[0] instanceof NetCDFRasterDataset;
+		}
+		return false;
+	}
+
+	/**
+	 * Gestiona la apertura del dialogo de propiedades del NetCDF
+	 * 
+	 * @param item
+	 *            elemento del menu
+	 * @param selectedItems
+	 *            conjunto de capas seleccionadas
+	 * @see com.iver.cit.gvsig.project.documents.view.toc.AbstractTocContextMenuAction
+	 *      #execute(com.iver.cit.gvsig.project.documents.view.toc.ITocItem,
+	 *      com.iver.cit.gvsig.fmap.layers.FLayer[])
 	 */
 	@Override
 	public void execute(ITocItem item, FLayer[] selectedItems) {
 		if ((selectedItems == null) || (selectedItems.length != 1))
-            return ;
-        FLayer lyr = selectedItems[0];
+			return;
+		FLayer lyr = selectedItems[0];
 
 		try {
 			enableEvents = false;
@@ -137,76 +212,21 @@ public class PropertiesNetCDFRasterTocMenuEntry extends 	AbstractTocContextMenuA
 			manager.setDefaultType(TabbedPanel.class);
 
 			TabbedPanel panelGroup = (TabbedPanel) manager.getPanelGroup(lyr);
-			PanelGroupLoaderFromExtensionPoint loader = new PanelGroupLoaderFromExtensionPoint("NetCDFPropertiesDialog");
+			PanelGroupLoaderFromExtensionPoint loader = new PanelGroupLoaderFromExtensionPoint(
+					"NetCDFPropertiesDialog");
 
-			properties = new PanelGroupDialog(lyr.getName() ,PluginServices.getText(this,"NetCDF_properties"), 550, 450, (byte) (WindowInfo.MODELESSDIALOG | WindowInfo.RESIZABLE | WindowInfo.MAXIMIZABLE), panelGroup);
+			properties = new PanelGroupDialog(lyr.getName(),
+					PluginServices.getText(this, "NetCDF_properties"), 550,
+					450, (byte) (WindowInfo.MODELESSDIALOG
+							| WindowInfo.RESIZABLE | WindowInfo.MAXIMIZABLE),
+					panelGroup);
 			properties.loadPanels(loader);
 			enableEvents = true;
 			RasterToolsUtil.addWindow(properties);
 		} catch (Exception e) {
 			RasterToolsUtil.messageBoxInfo("error_props_tabs", this, e);
-		} finally  {
+		} finally {
 			enableEvents = true;
 		}
 	}
-
-	/* (non-Javadoc)
-	 * @see org.gvsig.raster.gui.IGenericToolBarMenuItem#getIcon()
-	 */
-	@Override
-	public Icon getIcon() {
-		return RasterToolsUtil.getIcon("properties-icon");
-	}
-	
-	/**
-     * Verifica si el menú puede habilitarse o no
-     *
-     * @param item
-     *            elemento del menu
-     * @param selectedItems
-     *            conjunto de capas seleccionadas
-     *
-     * @return <b>true</b> si la capa seleccionada es un raster de un NetCDF<br />
-     *         <b>false</b> en cualquier otro caso
-     *
-     * @see com.iver.cit.gvsig.project.documents.view.toc.AbstractTocContextMenuAction#isVisible(com.iver.cit.gvsig.project.documents.view.toc.ITocItem,
-     *      com.iver.cit.gvsig.fmap.layers.FLayer[])
-     */
-	public boolean isEnabled(ITocItem item, FLayer[] selectedItems) {
-		if ((selectedItems == null) || (selectedItems.length != 1))
-            return false;
-        FLayer lyr = selectedItems[0];
-        if (lyr instanceof FLyrRasterSE) {
-        	FLyrRasterSE fr = (FLyrRasterSE) lyr;
-            return fr.getDataSource().getDataset(0)[0] instanceof NetCDFRasterDataset;
-        }
-        return false;
-    }
-	
-	/**
-     * Verifica si el menú puede mostrarse o no
-     *
-     * @param item
-     *            elemento del menu
-     * @param selectedItems
-     *            conjunto de capas seleccionadas
-     *
-     * @return <b>true</b> si la capa seleccionada es un raster de un NetCDF<br />
-     *         <b>false</b> en cualquier otro caso
-     *
-     * @see com.iver.cit.gvsig.project.documents.view.toc.AbstractTocContextMenuAction#isVisible(com.iver.cit.gvsig.project.documents.view.toc.ITocItem,
-     *      com.iver.cit.gvsig.fmap.layers.FLayer[])
-     */
-    public boolean isVisible(ITocItem item, FLayer[] selectedItems) {
-        if ((selectedItems == null) || (selectedItems.length != 1))
-            return false;
-        FLayer lyr = selectedItems[0];
-        if (lyr instanceof FLyrRasterSE) {
-            FLyrRasterSE fr = (FLyrRasterSE) lyr;
-            return fr.getDataSource().getDataset(0)[0] instanceof NetCDFRasterDataset;
-          }
-        return false;
-    }
-
-	
 }
